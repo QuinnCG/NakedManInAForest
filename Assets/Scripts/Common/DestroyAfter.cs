b4eeb5353526cb7f54baf1cca6d1d370f2346a7a
Quinton <86534924+QuinnCG@users.noreply.github.com>
2024-02-17 21:26:27 -0500

    Changed hurt shader to red

--- Assets/Scripts/Common/DestroyAfter.cs
+++ Assets/Scripts/Common/DestroyAfter.cs
@@ -1,14 +1,34 @@
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 namespace Quinn
 {
 	public class DestroyAfter : MonoBehaviour
 	{
+		private static bool _firstSpawn = true;
+
 		public float Delay = 1f;
 
 		private void Start()
 		{
-			Destroy(gameObject, Delay);
+			if (_firstSpawn)
+			{
+				_firstSpawn = false;
+			}
+			else
+			{
+				Destroy(gameObject);
+				return;
+			}
+
+			DOVirtual.DelayedCall(Delay, () =>
+			{
+				GetComponent<TextMeshProUGUI>().DOFade(0f, 1f).onComplete += () =>
+				{
+					Destroy(gameObject, Delay);
+				};
+			}, ignoreTimeScale: false);
 		}
 	}
 }
